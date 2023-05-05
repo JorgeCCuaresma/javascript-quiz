884b9449d0671a655d562783dd934982916f2492
Jorge Cuesta <jorgecuesta.90@gmail.com>
2023-05-05 14:17:18 +0200

    game completed

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,12 +1,25 @@
-import { Card, Typography, ListItem, List, ListItemButton, ListItemText } from '@mui/material'
+import { Card, Typography, ListItem, List, ListItemButton, ListItemText, Stack, IconButton } from '@mui/material'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import { gradientDark } from 'react-syntax-highlighter/dist/esm/styles/hljs'
 import { type Question as Questiontype } from '../types'
 import { useQuestionsStore } from '../store/questions'
+import { ArrowBackIosNew, ArrowForwardIos } from '@mui/icons-material'
+import { Footer } from './Footer'
 
 const Question = ({ info }: { info: Questiontype }) => {
+  const selectAnswer = useQuestionsStore(state => state.selectAnswer)
+
+  const getBackgroundColor = (index: number) => {
+    const { userSelectedAnswer, correctAnswer } = info
+    if (userSelectedAnswer == null) return 'transparent'
+    if (index !== correctAnswer && index !== userSelectedAnswer) return 'transparent'
+    if (index === correctAnswer) return 'green'
+    if (index === userSelectedAnswer) return 'red'
+    return 'transparent'
+  }
+
   return (
-    <Card variant='outlined' sx={{ textAlign: 'left', bgcolor: '#222', p: 2, marginTop: 4, width: 600 }}>
+    <Card variant='outlined' sx={{ textAlign: 'left', bgcolor: '#222', p: 2, marginTop: 4 }}>
       <Typography variant='h5'>
         {info.question}
       </Typography>
@@ -16,7 +29,12 @@ const Question = ({ info }: { info: Questiontype }) => {
       <List sx={{ bgcolor: '#333' }} disablePadding>
         {info.answers.map((answer, index) => (
           <ListItem key={index}>
-            <ListItemButton divider>
+            <ListItemButton
+              divider
+              onClick={() => selectAnswer(info.id, index)}
+              sx={{ bgcolor: getBackgroundColor(index) }}
+              disabled={info.userSelectedAnswer != null}
+            >
               <ListItemText sx={{ textAlign: 'center' }} primary={answer} />
             </ListItemButton>
           </ListItem>
@@ -29,12 +47,28 @@ const Question = ({ info }: { info: Questiontype }) => {
 export const Game = () => {
   const questions = useQuestionsStore(state => state.questions)
   const currentQuestion = useQuestionsStore(state => state.currentQuestion)
+  const goNextQuestion = useQuestionsStore(state => state.goNextQuestion)
+  const goPreviousQuestion = useQuestionsStore(state => state.goPrevQuestion)
 
   const questionInfo = questions[currentQuestion]
+  console.log(questions)
 
   return (
     <>
+      <Stack direction='row' gap={2} alignItems='center' justifyContent='center' marginTop={4}>
+        <IconButton onClick={goPreviousQuestion} disabled={currentQuestion === 0}>
+          <ArrowBackIosNew />
+        </IconButton>
+
+        {currentQuestion + 1} / {questions.length}
+
+        <IconButton onClick={goNextQuestion} disabled={currentQuestion === questions.length - 1}>
+          <ArrowForwardIos />
+        </IconButton>
+      </Stack>
       <Question info={questionInfo} />
+      <Footer />
+
     </>
   )
 }
